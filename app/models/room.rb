bf8f55a33d6a93712da5876d8873cfa80a8073c7
Udo Schmidt Junior <udo.schmidt.jr@gmail.com>
2022-12-15 18:58:01 -0300

    add scroll reset and text reset

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,13 +2,16 @@ class Room < ApplicationRecord
   validates_uniqueness_of :name
   scope :public_rooms, -> { where(is_private: false) }
   after_create_commit { broadcast_if_public }
+  # after_create_commit { broadcast_append_to "rooms" }
+
+
 
   has_many :messages
   has_many :participants, dependent: :destroy
 
 
   def broadcast_if_public
-    broadcast_append_to "room" unless self.is_private
+    broadcast_append_to "rooms" unless self.is_private
   end
 
   def self.create_private_room(users, room_name)
